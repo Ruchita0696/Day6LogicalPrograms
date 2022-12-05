bbb569401d9da9729cbe3dbfd960b37e17e7bd0f
ruchi06 <ruchi06@gmail.com>
2022-12-05 18:36:05 +0530

    Fibonacci Series

---
+++ Day6Problems/Day6Problems/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day6Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day6Problems num = new Day6Problems();
+            num.Fibonacci_Series();
+        }
+    }
+}
